9fdb3e807309d1288cd833476074b9b24f22d135
Jahidul Islam <jahid.redmi10@gmail.com>
2025-07-17 21:10:57 +0600

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
-import { User } from "./user.model";
+
 import hthpStatus from "http-status";
+import userServicecs from "./user.services";
 const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-    const user = await User.create({
-      name,
-      email,
-    });
+    const user = await userServicecs.createUser(req.body);
     res.status(hthpStatus.CREATED).json({
       success: true,
       massage: "user create success",
